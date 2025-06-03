ff97960d586fa999eb1d19ed78bee5ce0e2c399b
月白 <yueby_sp@qq.com>
2025-06-04 01:38:05 +0800

    Update x-draw.user.js

--- x-draw.user.js
+++ x-draw.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         X Draw Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  X Draw Helper, helps to quickly conduct draws
 // @author       Yueby
 // @match        https://x.com/*
